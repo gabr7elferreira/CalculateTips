7e1efcd96632c89cac9dcc7eafcd4c6286c7e3e5
gabr7elferreira <W$w9AUoy6BB!@#be$90%@5%*&8BH2T^Bkos%V3#DJ2*fQ$C9a>
2025-02-12 02:38:55 -0300

    update information about task

--- app/src/main/java/com/Gabriel/beTrend/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/Gabriel/beTrend/tipcalculator/MainActivity.kt
@@ -6,6 +6,28 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+/*
+
+PROGRAMA PARA CALCULAR AS GORGETAS
+
+ETAPA 01:
+- Criar um titulo
+- criar um subtitulo
+- criar um campo para add o valor da refeicao
+- criar um campo para add o numero de pessoas que comeram
+- adicionar as opcoes com o percentual para a gorgeta: ex 15% 20% 30%
+- adicionar o button para limpar os dados inseridos
+- adiconar o button para calcular o valor da gorgeta
+----------------------------------------------------------
+
+ETAPA 02:
+
+- Recuperar as Views do Layout XML
+    - findViewByID
+    - ViewBinding
+
+*/
+
 class MainActivity: AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
